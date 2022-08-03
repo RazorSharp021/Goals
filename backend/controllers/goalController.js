1f59d8426b5c506b25aa27100fa79c225a45e404
RazorSharp021 <reesarmani@gmail.com>
2022-08-04 09:52:24 +1000

    controller update

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,13 @@
+// @desc    Get goals
+// @route   GET api/goals
+// @access  Private
 const getGoals = (req, res) => {
   res.json({ message: 'Get goals' })
 }
 
+// @desc    Post goals
+// @route   POST api/goals
+// @access  Private
 const postGoals = (req, res) => {
   if(!req.body.text) {
     res.status(400)
@@ -10,10 +16,16 @@ const postGoals = (req, res) => {
   res.json({message: 'Set goals'})
 }
 
+// @desc    Update goals
+// @route   PUT api/goals/:id
+// @access  Private
 const putGoals = (req, res) => {
   res.json({message: `Update goal ${req.params.id}`})
 }
 
+// @desc    Delete goals
+// @route   DELETE api/goals/:id
+// @access  Private
 const deleteGoals = (req, res) => {
   res.json({message: `Update goal ${req.params.id}`})
 }
