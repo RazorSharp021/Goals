9374988e00ff10cf7814bf86c36beb59c9ebf5c1
RazorSharp021 <reesarmani@gmail.com>
2022-08-06 18:20:58 +1000

    async handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,34 +1,38 @@
+// In order to use mongoose, we need to use asyncHandler, as the database is going to deal with promises 
+const asyncHandler = require('express-async-handler')
+
+
 // @desc    Get goals
 // @route   GET api/goals
 // @access  Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
   res.json({ message: 'Get goals' })
-}
+})
 
-// @desc    Post goals
+// @desc    Set goals
 // @route   POST api/goals
 // @access  Private
-const postGoals = (req, res) => {
+const postGoals = asyncHandler(async(req, res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('please add a text field') 
   }
   res.json({message: 'Set goals'})
-}
+})
 
 // @desc    Update goals
 // @route   PUT api/goals/:id
 // @access  Private
-const putGoals = (req, res) => {
+const putGoals = asyncHandler(async(req, res) => {
   res.json({message: `Update goal ${req.params.id}`})
-}
+})
 
 // @desc    Delete goals
 // @route   DELETE api/goals/:id
 // @access  Private
-const deleteGoals = (req, res) => {
+const deleteGoals = asyncHandler(async(req, res) => {
   res.json({message: `Delete goal ${req.params.id}`})
-}
+})
 
 module.exports = {
   getGoals, 
