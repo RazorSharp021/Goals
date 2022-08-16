52bf8d5404108176f47b67ae7cbc93086e6bb9e4
RazorSharp021 <reesarmani@gmail.com>
2022-08-17 09:29:02 +1000

    jwt implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,7 +13,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400)
     throw new Error ('Please add all fields')
   }
-  // Check if user exists 
+  //Check if user exists 
   const userExists = await User.findOne({email})
 
   if(userExists) {
@@ -35,7 +35,8 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(201).json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user._id)
     })
   } else {
     res.status(400)
@@ -47,7 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: 'Login User'})
+  const { email, password } = req.body
+  //Check for user email
+  const user = await User.findOne({email})
+
+  if(user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid email or password')
+  }
 })
 
 // @desc    Get user data 
@@ -56,6 +71,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data display'})
 })
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({id}, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+}
 
 module.exports = {
   registerUser,
